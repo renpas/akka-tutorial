571a251678f515e16211189f2a5719e5e7ce8d3f
Renã Passos de Souza <ren.boxe@gmail.com>
2017-06-18 18:28:43 -0300

    DeviceManager new spec

--- src/main/scala/iot/DeviceManager.scala
+++ src/main/scala/iot/DeviceManager.scala
@@ -1,22 +1,26 @@
 package iot
 
-import akka.actor.Actor.Receive
 import akka.actor._
-import iot.DeviceManager.RequestTrackDevice
+import iot.DeviceManager.{ReplyDeviceGroupList, RequestDeviceGroupList, RequestTrackDevice}
 
 /**
   * @author rsouza on 17/06/17.
   */
 object DeviceManager {
 
-  def props(): Props = Props[DeviceManager]
+  def props(): Props = Props(new DeviceManager(deviceGroupFactory))
+
+  def deviceGroupFactory(context:ActorContext, groupId:String) = context.actorOf(DeviceGroup.props(groupId))
 
   final case class RequestTrackDevice(groupId: String, deviceId: String)
   case object DeviceRegistered
 
+  final case class RequestDeviceGroupList(requestId: Long)
+  final case class ReplyDeviceGroupList(requestId:Long, ids: Set[String])
+
 }
 
-class DeviceManager extends Actor with ActorLogging {
+class DeviceManager(deviceGroupFactory: (ActorContext, String) => ActorRef) extends Actor with ActorLogging {
   var groupIdToActor = Map.empty[String, ActorRef]
   var actorToGroupId = Map.empty[ActorRef, String]
 
@@ -31,13 +35,15 @@ class DeviceManager extends Actor with ActorLogging {
           ref forward trackMsg
         case None =>
           log.info("Creating device group actor for {}", groupId)
-
-          val groupActor = context.actorOf(DeviceGroup.props(groupId))
+          val groupActor:ActorRef = deviceGroupFactory(context, groupId)
           context.watch(groupActor)
           groupActor forward trackMsg
           groupIdToActor += groupId -> groupActor
           actorToGroupId += groupActor -> groupId
       }
+
+    case RequestDeviceGroupList(requestId) =>
+      sender ! ReplyDeviceGroupList(requestId, groupIdToActor.keySet)
 
     case Terminated(groupActor) =>
       val groupId = actorToGroupId(groupActor)
--- src/test/scala/iot/DeviceManagerSpec.scala
+++ src/test/scala/iot/DeviceManagerSpec.scala
@@ -2,7 +2,9 @@ package iot
 
 import akka.actor._
 import akka.testkit._
+import iot.DeviceManager.ReplyDeviceGroupList
 import org.scalatest._
+
 import scala.concurrent.duration._
 
 /**
@@ -26,6 +28,10 @@ class DeviceManagerSpec extends FlatSpec with Matchers {
     val deviceActor2 = probe.lastSender
     deviceActor1 should not be deviceActor2
 
+    managerActor.tell(DeviceManager.RequestDeviceGroupList(0), probe.ref)
+    val deviceList = probe.expectMsgType[DeviceManager.ReplyDeviceGroupList]
+    deviceList.ids should be(Set("group"))
+
     // check that the device actors are working
     deviceActor1.tell(Device.RecordTemperature(requestId = 0, 1.0), probe.ref)
     probe.expectMsg(Device.TemperatureRecorded(requestId = 0))
@@ -33,5 +39,40 @@ class DeviceManagerSpec extends FlatSpec with Matchers {
     probe.expectMsg(Device.TemperatureRecorded(requestId = 1))
   }
 
+  it should " be able to list groups even after removing one " in {
+    val probe = TestProbe()
+
+    var groupIdToDeviceGroupRef = Map.empty[String, ActorRef]
+
+    val deviceGroupFactory = (context:ActorContext, groupId:String) => {
+      val deviceGroupRef = context.actorOf(DeviceGroup.props(groupId))
+
+      groupIdToDeviceGroupRef += groupId -> deviceGroupRef
+
+      deviceGroupRef
+    }
+
+    val managerActor = system.actorOf(Props( new DeviceManager(deviceGroupFactory)))
+
+    managerActor.tell(DeviceManager.RequestTrackDevice("group1", "device1"), probe.ref)
+    probe.expectMsg(DeviceManager.DeviceRegistered)
+    managerActor.tell(DeviceManager.RequestTrackDevice("group1", "device2"), probe.ref)
+    probe.expectMsg(DeviceManager.DeviceRegistered)
+    managerActor.tell(DeviceManager.RequestTrackDevice("group2", "device1"), probe.ref)
+    probe.expectMsg(DeviceManager.DeviceRegistered)
+
+    managerActor.tell(DeviceManager.RequestDeviceGroupList(0), probe.ref)
+    val deviceList1 = probe.expectMsgType[DeviceManager.ReplyDeviceGroupList]
+
+    deviceList1 should be(ReplyDeviceGroupList(0, Set("group1", "group2")))
+
+    groupIdToDeviceGroupRef("group2") ! PoisonPill
+
+    probe.awaitAssert{
+      managerActor.tell(DeviceManager.RequestDeviceGroupList(1), probe.ref)
+      val deviceList2 = probe.expectMsgType[DeviceManager.ReplyDeviceGroupList]
+      deviceList2 should ===(ReplyDeviceGroupList(1, Set("group1")))
+    }
+  }
 
 }
